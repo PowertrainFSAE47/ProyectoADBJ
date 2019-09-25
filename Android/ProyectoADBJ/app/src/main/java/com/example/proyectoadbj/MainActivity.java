589041bfe003f7adc6c8e064a926e026fc2da1ef
SirTanji <bjaraschwalm1@gmail.com>
2019-09-25 16:17:06 -0300

    Aca van 3 pantallas extra, sin funcionalidad.

--- Android/ProyectoADBJ/app/src/main/java/com/example/proyectoadbj/MainActivity.java
+++ Android/ProyectoADBJ/app/src/main/java/com/example/proyectoadbj/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.proyectoadbj;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
 
