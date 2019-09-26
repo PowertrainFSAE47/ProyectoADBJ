e0f06e599c86a6ae4794e45fa0d81671869c7b16
Angelo <dominicis.a@gmail.com>
2019-09-26 00:12:11 -0300

    Agregada primera transición de pantallas, corregido falta de clases java en activity 2

--- Android/ProyectoADBJ/app/src/main/java/com/example/proyectoadbj/MainActivity.java
+++ Android/ProyectoADBJ/app/src/main/java/com/example/proyectoadbj/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.proyectoadbj;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -41,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
                     // Tiene user y tiene clave
                     Toast.makeText(MainActivity.this,"WENA PERRO",Toast.LENGTH_SHORT).show();
 
+                    Intent aMenuPrincipal = new Intent(MainActivity.this, menuPrincipal.class);
+                    startActivity(aMenuPrincipal);
 
                 }
             }
