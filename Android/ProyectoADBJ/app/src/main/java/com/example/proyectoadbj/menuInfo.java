aa4d6926660294c7190ffd0a242cdb4b83a5403b
SirTanji <bjaraschwalm1@gmail.com>
2019-09-26 14:36:40 -0300

    Cambio pantalla planes, boton pantalla info y detalles.

--- Android/ProyectoADBJ/app/src/main/java/com/example/proyectoadbj/menuInfo.java
+++ Android/ProyectoADBJ/app/src/main/java/com/example/proyectoadbj/menuInfo.java
@@ -3,12 +3,41 @@ package com.example.proyectoadbj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
 
 public class menuInfo extends AppCompatActivity {
+
+    private ToggleButton tbtMapa;
+    private ImageView imgMapa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_info);
+
+        tbtMapa = (ToggleButton) findViewById(R.id.tbtMapa);
+        imgMapa = (ImageView) findViewById(R.id.imgMapa);
+        botonMapa();
+        imgMapa.setVisibility(View.INVISIBLE);
+
+    }
+
+    private void botonMapa(){
+        tbtMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int img = 0;
+                tbtMapa.setSelected(false);
+                img = getResources().getIdentifier("@drawable/foto8", null, getPackageName());
+                if (tbtMapa.isChecked()){
+                    imgMapa.setVisibility(View.VISIBLE);
+                }else{
+                    imgMapa.setVisibility(View.INVISIBLE);
+                }
+                imgMapa.setImageResource(img);
+            }
+        });
     }
 }
