19839c0e594d57b24e38d93c8ac7f1c813474df4
Angelo <dominicis.a@gmail.com>
2019-09-26 00:00:52 -0300

    Background de Menus, agregada primera validacion

--- Android/ProyectoADBJ/app/src/main/java/com/example/proyectoadbj/MainActivity.java
+++ Android/ProyectoADBJ/app/src/main/java/com/example/proyectoadbj/MainActivity.java
@@ -4,15 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btLogin;
+    private EditText txUser;
+    private EditText txPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Instanciar botones y controles
+
+        btLogin=(Button) findViewById(R.id.btLogin);
+        txUser=(EditText) findViewById(R.id.txUser);
+        txPassword=(EditText) findViewById(R.id.txPassword);
+
+        //Desplegar toast en caso de que no se llenen algunos campos
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (txUser.getText().toString().trim().isEmpty()){
+                    //No tiene user
+                    Toast.makeText(MainActivity.this,"No ingresó un usuario",Toast.LENGTH_SHORT).show();
+                }else if (txPassword.getText().toString().trim().isEmpty()){
+                    // No tiene password
+                    Toast.makeText(MainActivity.this,"No ingresó un password",Toast.LENGTH_SHORT).show();
+                }else{
+                    // Tiene user y tiene clave
+                    Toast.makeText(MainActivity.this,"WENA PERRO",Toast.LENGTH_SHORT).show();
+
+
+                }
+            }
+        });
+
+
+
     }
 }
 
-// Comentario agregado desde github
