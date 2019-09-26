9868a3559bd1368692e21279a0ad900854a75a42
SirTanji <bjaraschwalm1@gmail.com>
2019-09-26 18:55:20 -0300

    Pantalla nueva cuenta, pantalla listado de suscripcion, detalles.

--- Android/ProyectoADBJ/app/src/main/java/com/example/proyectoadbj/menuDatosUsuario.java
+++ Android/ProyectoADBJ/app/src/main/java/com/example/proyectoadbj/menuDatosUsuario.java
@@ -3,12 +3,22 @@ package com.example.proyectoadbj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class menuDatosUsuario extends AppCompatActivity {
+
+    private ListView lvDatosSubs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_datos_usuario);
+
+        lvDatosSubs = (ListView) findViewById(R.id.lvDatosSub);
+        ArrayList <String> lista = new ArrayList<String>();
+
     }
 }
