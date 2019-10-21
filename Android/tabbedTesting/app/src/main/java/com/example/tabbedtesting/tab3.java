2611e752f95339ef14b5d3c8eb84832b6407369c
Angelo <dominicis.a@gmail.com>
2019-10-20 22:54:22 -0300

    Limpieza de directorio

--- Android/tabbedTesting/app/src/main/java/com/example/tabbedtesting/tab3.java
+++ Android/tabbedTesting/app/src/main/java/com/example/tabbedtesting/tab3.java
@@ -13,10 +13,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class tab1 extends Fragment {
+public class tab3 extends Fragment {
 
 
-    public tab1() {
+    public tab3() {
         // Required empty public constructor
     }
 
@@ -25,7 +25,7 @@ public class tab1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab1, container, false);
+        return inflater.inflate(R.layout.fragment_tab3, container, false);
     }
 
 }
