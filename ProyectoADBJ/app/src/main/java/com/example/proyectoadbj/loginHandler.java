30e63b0abcdf23569307bcec11946fb51337759b
PowertrainFSAE47 <dominicis.a@gmail.com>
2019-11-09 21:31:01 -0300

    Agregada base de datos, agregado check de usuario.

---
+++ ProyectoADBJ/app/src/main/java/com/example/proyectoadbj/loginHandler.java
@@ -0,0 +1,15 @@
+package com.example.proyectoadbj;
+
+public class loginHandler {
+
+    // Esta clase verifica la existencia de un usuario en la base de datos.
+
+
+    public loginHandler() {
+    }
+
+    public boolean checkUser(String user, String pass)
+    {
+        return false;
+    }
+}
